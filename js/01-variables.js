2a9f25080f8ad5b0e3e8abe56925b54dfdac3e46
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-09-06 12:01:46 +0300

    variables

---
+++ js/01-variables.js
@@ -0,0 +1,25 @@
+// kad narsyklė isimintu mūsu vardą, bent jau tuo metu:
+
+ const vardas = 'Vardenis'; // stringas
+ const amzius = 99;  // skai2ius
+ const buvoDuse = true; // boolean
+ const dabarYraNaktis = false;  // boolean
+ const dabarSninga = true;  // boolean
+ const pazymiai = [5, 2, 10, 8, 7]; //array
+ const draugai = [Dalia, Antanas, Sniega, Jonas]; //array
+ const Dalia = {
+     vardas: 'Dalia',
+     amzius: 40,
+     megstamaSpalva: 'black',
+     baigtaKlasiu: 12,
+     gyvenamojiVieta = {  //vel viduje objektas
+        miestas: 'Palanga',
+        gatve: 'Basanaviciaus',
+        namoNumeris: 1
+
+     }
+ }
+
+ const neigiamiSkaiciai = [-5, -8, -0,001];
+ const pazymys1 = 5;
+ const pazymys_2 = 2
